9e3e76208885a1229f2c1e59c04fc03edced2d29
Gabriel Pedreiro Crepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-11-16 17:31:34 -0300

    começando

---
+++ Projetos_Python/Ex001.py
@@ -0,0 +1,4 @@
+nome = input('Digite o seu nome: ')
+idade = input('Digite a sua Idade: ')
+peso = input('Digite o seu peso: ')
+print(f'o seu nome é {nome}, sua idade é {idade} e seu peso é {peso}, correto?')
