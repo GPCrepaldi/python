11fd0dde6365823361de3a0c8d48d9cbc3192a59
Gabriel Pedreiro Crepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-11-16 17:55:23 -0300

    primeiras aulas e ajustes

---
+++ Projetos_Python/aula00a.py
@@ -0,0 +1,12 @@
+n1 = input('Digite o valor de n1: ')
+print(type(n1))
+
+n2 = int(input('Digite o valor de n2: '))
+print(type(n2))
+
+n3 = int(input('Digite o valor de n3: '))
+print(type(n3))
+
+soma = n2 + n3
+
+print(f'A soma entre {n2} e {n3} é de {soma}')
--- script - python/Desafio3_soma.py
+++ script - python/Desafio3_soma.py
@@ -1,4 +1,4 @@
-numero1=input('Digite o primeiro numero: ')
-numero2=input('Digite o segundo numero: ')
+numero1 = int(input('Digite o primeiro numero: '))
+numero2 = int(input('Digite o segundo numero: '))
 soma = (numero1 + numero2)
 print(f'a soma do numero {numero1} com o numero {numero2} é {soma}')
