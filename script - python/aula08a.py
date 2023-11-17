64fff503f4a107c1002afffda3d005c33150f985
Gabriel Pedreiro Crepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-11-17 09:01:17 -0300

    aula08a

---
+++ script - python/aula08a.py
@@ -0,0 +1,9 @@
+from math import sqrt
+import random
+num = int(input('Digite um numero: '))
+raiz = sqrt(num)
+print(f'A raiz de {num} é de {raiz:.2f}')
+
+print(f'Gerando um numero aleatório: ')
+num = random.randint(1, 10)
+print(num)
