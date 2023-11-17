0a04a6b7cac787de3364d69c7d2de317e259348c
Gabriel Pedreiro Crepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-11-17 06:38:33 -0300

    aula006

---
+++ Projetos_Python/aula06b.py
@@ -0,0 +1,15 @@
+n1 = bool(input('Digite algo: '))
+print(type(n1))
+print(n1)
+
+n2 = input('Digite algo numerico: ')
+print(type(n2))
+print(n2.isnumeric())
+
+n3 = input('Digite algo alfabético: ')
+print(type(n3))
+print(n3.isalpha())
+
+n4 = input('Digite algo alfanumérico: ')
+print(type(n4))
+print(n4.isalnum())
