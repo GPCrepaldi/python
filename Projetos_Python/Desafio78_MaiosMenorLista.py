06472119cdb6f8579907158898cef4cb02d920e9
Gabriel Pedreiro Crepaldi <gabicrepa@hotmail.com>
2024-09-11 17:00:30 -0300

    desafio 78 feito

---
+++ Projetos_Python/Desafio78_MaiosMenorLista.py
@@ -0,0 +1,16 @@
+numeros = []
+
+for n in range(0, 5):
+    numeros.append(int(input('Digite o valor a ser adicionado: ')))
+print(numeros)
+
+
+print(f'\nO maior valor foi {max(numeros)} na posição(ões): ', end='')
+for i, valor in enumerate(numeros): #neste caso eu n pude usar o index, pq o index apenas retorno a primeira ocorrencia do valor
+    if valor == max(numeros):
+        print(f'{i} ', end='')
+
+print(f'\nO menor valor foi {min(numeros)} na posição(ões): ', end='')
+for i, valor in enumerate(numeros): #sendo assim, é melhor usar o enumerate, pq ele vai numerando todo vetor(lista)
+    if valor == min(numeros):
+        print(f'{i} ', end='')
